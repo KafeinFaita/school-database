447e89ebdab9d2e596e5211031017db20ed3c7a9
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2021-02-24 23:39:40 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express();
+
+const PORT = process.env.port || 3000;
+
+app.listen(PORT)
+
+app.get('/', (req, res) => {
+    res.send('HELLO')
+})
