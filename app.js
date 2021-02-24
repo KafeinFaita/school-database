0b39eea0a4bc73d47cc77abacfd2ba03d297d3ae
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2021-02-25 00:54:14 +0800

    implemented mvc

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express')
+const mainRoutes = require('./routes/mainRoutes')
 
 const app = express();
 
 app.use(express.static(__dirname + "/public"))
 app.set('view engine', 'ejs')
 
-const PORT = process.env.port || 3000;
+app.use(mainRoutes)
 
+const PORT = process.env.port || 3000;
 app.listen(PORT)
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+
 
