c2c242efe4e16d80acbabb6c0cabcb7fe791ef17
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2021-02-25 00:21:09 +0800

    added ejs view engine

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require('express')
 const app = express();
 
 app.use(express.static(__dirname + "/public"))
+app.set('view engine', 'ejs')
 
 const PORT = process.env.port || 3000;
 
 app.listen(PORT)
 
 app.get('/', (req, res) => {
-    res.send('HELLO')
+    res.render('index')
 })
 
