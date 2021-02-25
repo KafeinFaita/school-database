4138129f2a47aea26d691d40ee1d785f0ef9599b
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2021-02-25 22:46:08 +0800

    basta maraming binago

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,15 +1,18 @@
-const Grade = require('../models/Grade')
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 module.exports.index_get = (req, res) => {
     res.render('index');
 }
 
-module.exports.index_post = async (req, res) => {
-    const grade = new Grade(req.body)
-    
-    try{
-        const saveGrade = await grade.save()
-        res.json(req.body)
+module.exports.login_post = async (req, res) => {   
+    try {
+        const hashedPass = await bcrypt.hash(req.body.password, 10)
+        console.log(hashedPass)
+        const user = new User({ username: req.body.username, password: hashedPass })
+
+        const saveGrade = await user.save()
+        res.send(req.body)
     }
     catch(err) {
         res.send(err)
@@ -18,4 +21,8 @@ module.exports.index_post = async (req, res) => {
 
 module.exports.login_get = (req, res) => {
     res.render('login')
+}
+
+module.exports.register_get = (req, res) => {
+    res.render('register')
 }
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { index_get, login_get, index_post } = require('../controllers/mainController') 
+const { index_get, login_get, login_post, register_get } = require('../controllers/mainController') 
 
 const router = express.Router()
 
 router.get('/', index_get);
+router.get('/register', register_get)
 router.get('/login', login_get);
-router.post('/', index_post)
+router.post('/login', login_post)
 
 module.exports = router;
