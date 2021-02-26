35034289f61f661c3267c262188b25211bb7bf73
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2021-02-27 02:20:08 +0800

    added search function

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const StudentRecord = require('../models/StudentRecord')
 const bcrypt = require('bcrypt')
 
 //GET requests
@@ -15,9 +16,34 @@ module.exports.register_get = (req, res) => {
     res.render('register')
 }
 
-module.exports.studentRecord_get = (req, res) => {
-    res.render('student-record')
+module.exports.studentRecord_get = async (req, res) => {
+
+    if (Object.keys(req.query) !== 0) {
+        try {
+            const searchName = await StudentRecord.find({ firstname: new RegExp(`^${req.query.name}$`,'i')})
+            res.render('student-record', { record: searchName })
+        } catch(err) {
+            console.log(err)
+        }
+        
+    } else {
+        console.log(req.query)
+        res.render('student-record')
+    }  
 }
+
+module.exports.studentRecord_get_one = async (req, res) => {
+    
+    try {
+        const getStudent = await StudentRecord.findById(req.params.id)
+        console.log(getStudent)
+        res.render('student-record-one', { student: getStudent })
+    } catch (err) {
+        console.log(err)
+    }
+    
+}
+
 
 //POST requests
 
@@ -33,5 +59,13 @@ module.exports.login_post = async (req, res) => {
     catch(err) {
         res.send(err)
     }
+}
+
+module.exports.student_record_post = async (req, res) => {   
+    const record = new StudentRecord(req.body)
+
+    const saveRecord = await record.save()
+    res.json(record)
+    
 }
 
--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { index_get, login_get, login_post, register_get, studentRecord_get } = require('../controllers/mainController') 
+const { index_get, login_get, login_post, register_get, studentRecord_get, student_record_post, studentRecord_get_one } = require('../controllers/mainController') 
 
 const router = express.Router()
 
@@ -7,6 +7,9 @@ router.get('/', index_get);
 router.get('/register', register_get)
 router.get('/login', login_get);
 router.get('/student-record', studentRecord_get)
+router.get('/student-record/:id', studentRecord_get_one)
+
 router.post('/login', login_post)
+router.post('/student-record', student_record_post)
 
 module.exports = router;
