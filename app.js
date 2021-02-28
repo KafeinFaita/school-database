a30b1c924628f4ef35889476f63e1afa378f91ee
paulpdoa <polopdoandres@gmail.com>
2021-02-28 23:46:51 +0800

    test form page

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const mainRoutes = require('./routes/mainRoutes')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-
 const app = express();
 
 app.use(express.static(__dirname + "/public"))
