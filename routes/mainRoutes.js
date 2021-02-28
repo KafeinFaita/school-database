2fe3270bece23726401fcc2b35cf1b1f49c35987
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2021-02-28 18:02:43 +0800

    various bug fixes

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,15 +1,11 @@
 const express = require('express')
 const { 
-    index_get, 
-    login_get, 
-    login_post, 
-    register_get, 
-    studentRecord_get, 
-    errorPage_get,
-    student_record_post, 
-    studentRecord_get_one, 
-    studentRecord_put_one, 
-    studentRecord_delete_one } = require('../controllers/mainController') 
+    index_get, login_get, 
+    login_post, register_get, 
+    studentRecord_get, errorPage_get,
+    student_record_post, studentRecord_get_one, 
+    studentRecord_put_one, studentRecord_delete_one,
+    section_get, section_post } = require('../controllers/mainController') 
 
 const router = express.Router()
 
@@ -18,10 +14,12 @@ router.get('/register', register_get)
 router.get('/login', login_get);
 router.get('/student-record', studentRecord_get)
 router.get('/student-record/:id', studentRecord_get_one)
+router.get('/section', section_get)
 router.get('*', errorPage_get)
 
 router.post('/login', login_post)
 router.post('/student-record', student_record_post)
+router.post('/section', section_post)
 
 router.put('/student-record/:id', studentRecord_put_one)
 
