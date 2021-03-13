5427a518e8c2d2840357bde29ca0e1193877cf36
paulpdoa <polopdoandres@gmail.com>
2021-03-13 19:03:20 +0800

    tapos na yung naimport na class

--- public/scripts/siblingSearch.js
+++ public/scripts/siblingSearch.js
@@ -7,6 +7,22 @@ const siblingSearchResult = document.querySelector('.sibling-search-result')
 const siblingsList = document.querySelector('.siblings-list')
 
 const motherName = document.querySelector('.mothername')
+const motherOccupation = document.querySelector('.motheroccupation')
+const motherOffice = document.querySelector('.motheroffice')
+const motherContact = document.querySelector('.mothercontact')
+const motherEmail = document.querySelector('.motheremail')
+
+const fatherName = document.querySelector('.fathername')
+const fatherOccupation = document.querySelector('.fatheroccupation')
+const fatherOffice = document.querySelector('.fatheroffice')
+const fatherContact = document.querySelector('.fathercontact')
+const fatherEmail = document.querySelector('.fatheremail')
+
+const guardianName = document.querySelector('.guardianname')
+const guardianOccupation = document.querySelector('.guardianoccupation')
+const guardianOffice = document.querySelector('.guardianoffice')
+const guardianContact = document.querySelector('.guardiancontact')
+const guardianEmail = document.querySelector('.guardianemail')
 
 siblingRadio.forEach(radio => {
     radio.addEventListener('change', (e) => {
