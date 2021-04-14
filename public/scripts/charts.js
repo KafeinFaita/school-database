ff5ca0fcee875541f1f084c0be4b7e742416b7b7
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2021-04-14 19:39:43 +0800

    stuff

--- public/scripts/charts.js
+++ public/scripts/charts.js
@@ -1,7 +1,6 @@
 const doughnut = document.getElementById('doughnut-chart').getContext('2d')
 const bar = document.getElementById('bar-chart').getContext('2d')
-const line = document.getElementById('line-chart').getContext('2d')
-
+// const line = document.getElementById('line-chart').getContext('2d')
 
 const doughnutChart = new Chart(doughnut, {
     type: 'doughnut',
@@ -90,46 +89,39 @@ const barChart = new Chart(bar, {
     }
 });
 
-const lineChart = new Chart(line, {
-    type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: 'Sample Data',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        // scales: {
-        //     yAxes: [{
-        //         ticks: {
-        //             beginAtZero: true
-        //         }
-        //     }]
-        // },
-        title: {
-            display: true,
-            text: 'Bar Chart Sample'
-        },
-        responsive: false,
-        maintainAspectRatio: false
+// const lineChart = new Chart(line, {
+//     type: 'line',
+//     data: {
+//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//         datasets: [{
+//             label: 'Sample Data',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         title: {
+//             display: true,
+//             text: 'Bar Chart Sample'
+//         },
+//         responsive: false,
+//         maintainAspectRatio: false
 
-    }
-});
+//     }
+// });
